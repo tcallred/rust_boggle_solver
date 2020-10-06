7208f638bdac15c36569ce668559598d477e3d40
tcallred <taylor.allred21@gmail.com>
2020-10-06 10:25:19 -0600

    improved speed EXPONENTIALLY by using trie

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::fs;
 
-const MAX_WORD_LEN: usize = 12;
+#[derive(Debug)]
+struct Trie {
+    prefix: String,
+    is_word: bool,
+    children: HashMap<char, Trie>,
+}
+
+impl Trie {
+    pub fn new() -> Self {
+        Self {
+            prefix: "".to_string(),
+            is_word: false,
+            children: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, word: &str, prefix: String) {
+        if word.len() > 0 {
+            let ch = word.chars().nth(0).unwrap();
+            let new_prefix = format!("{}{}", prefix, ch);
+            if let Some(child) = self.children.get_mut(&ch) {
+                child.insert(&word[1..], new_prefix.clone());
+            } else {
+                let mut child = Self {
+                    prefix: new_prefix.clone(),
+                    is_word: false,
+                    children: HashMap::new(),
+                };
+                child.insert(&word[1..], new_prefix.clone());
+                self.children.insert(ch, child);
+            }
+        } else {
+            self.is_word = true;
+        }
+    }
+}
 
 #[derive(Clone, Debug, Copy)]
 struct Coordinate {
@@ -11,7 +46,7 @@ struct Coordinate {
 
 impl Coordinate {
     // Returns a new coordinate if the move is valid. None otherwise.
-    fn move_c(&self, dy: i64, dx: i64, h: i64, w: i64) -> Option<Coordinate> {
+    fn move_coord(&self, dy: i64, dx: i64, h: i64, w: i64) -> Option<Coordinate> {
         let new_row = self.row + dy;
         let new_col = self.col + dx;
 
@@ -32,54 +67,46 @@ impl PartialEq for Coordinate {
     }
 }
 
-fn boggle_solve(grid: &Vec<Vec<char>>, dict: &HashSet<String>) {
+fn boggle_solve(grid: &Vec<Vec<char>>, dict: &Trie) {
     for (i, row) in grid.iter().enumerate() {
         for (j, ch) in row.iter().enumerate() {
-            boggle_solve_help(
-                grid,
-                dict,
-                ch.to_string(),
-                Coordinate {
-                    row: i as i64,
-                    col: j as i64,
-                },
-                vec![Coordinate {
-                    row: i as i64,
-                    col: j as i64,
-                }],
-            )
+            if let Some(next_trie) = dict.children.get(ch) {
+                boggle_solve_help(
+                    grid,
+                    next_trie,
+                    Coordinate {
+                        row: i as i64,
+                        col: j as i64,
+                    },
+                    vec![Coordinate {
+                        row: i as i64,
+                        col: j as i64,
+                    }],
+                )
+            }
         }
     }
 }
 
-fn boggle_solve_help(
-    grid: &Vec<Vec<char>>,
-    dict: &HashSet<String>,
-    word: String,
-    c: Coordinate,
-    path: Vec<Coordinate>,
-) {
-    // Base case.
-    // No more work if the string is long enough
-    if word.len() > MAX_WORD_LEN {
-        return;
-    }
-
-    if word.len() > 3 && dict.contains(&word) {
-        println!("{}", word);
+fn boggle_solve_help(grid: &Vec<Vec<char>>, dict: &Trie, c: Coordinate, path: Vec<Coordinate>) {
+    if dict.is_word {
+        println!("{}", dict.prefix);
     }
 
     // For every possible direction the path can take, if it is valid, take it.
     for a in -1..=1 {
         for b in -1..=1 {
-            match c.move_c(a, b, grid.len() as i64, grid[0].len() as i64) {
+            match c.move_coord(a, b, grid.len() as i64, grid[0].len() as i64) {
                 Some(new_c) if !path.contains(&new_c) => {
-                    let mut new_path = path.clone();
-                    new_path.push(new_c);
-                    let new_word =
-                        format!("{}{}", &word, &grid[new_c.row as usize][new_c.col as usize]);
+                    if let Some(new_trie) = dict
+                        .children
+                        .get(&grid[new_c.row as usize][new_c.col as usize])
+                    {
+                        let mut new_path = path.clone();
+                        new_path.push(new_c);
 
-                    boggle_solve_help(grid, dict, new_word, new_c, new_path);
+                        boggle_solve_help(grid, new_trie, new_c, new_path);
+                    }
                 }
 
                 Some(_) => (),
@@ -92,7 +119,14 @@ fn boggle_solve_help(
 
 fn main() {
     let contents = fs::read_to_string("words_alpha.txt").unwrap();
-    let dict: HashSet<String> = contents.split_whitespace().map(|s| s.to_string()).collect();
+    // let dict: HashSet<String> = contents.split_whitespace().map(|s| s.to_string()).collect();
+    let mut dict = Trie::new();
+
+    for s in contents.split_whitespace() {
+        dict.insert(s, "".to_string())
+    }
+
+    // println!("{:?}", dict);
 
     let grid: Vec<Vec<char>> = vec![
         vec!['c', 's', 't', 'e', 't'],
